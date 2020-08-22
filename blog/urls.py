28ed98dbddf64f3626b24f16d2d200d6f2d170f8
Ruth-ikegah <ruthikegah@gmail.com>
2020-08-22 16:02:09 +0100

    Updates on blog
    Signed-off-by: Ruth-ikegah <ruthikegah@gmail.com>

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ app_name = 'blog'
 urlpatterns = [
     path('',HomePage.as_view(), name = "HomePage"),
     path('detail/<int:pk>/', ReadMore.as_view(), name = "detail-view"),
-    path('Share/', Share.as_view(success_url = '/'), name = "Share"),
+    path('Share/', Share.as_view(success_url = '../'), name = "Share"),
     
     
 ]
