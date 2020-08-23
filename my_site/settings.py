4559bfff38dabb2a19c4eebb664c96e50cba2fb9
Ruth-ikegah <ruthikegah@gmail.com>
2020-08-23 18:58:42 +0100

    Work in Progress
    Signed-off-by: Ruth-ikegah <ruthikegah@gmail.com>

--- my_site/settings.py
+++ my_site/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
