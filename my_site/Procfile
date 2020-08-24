657d89e8c030ec4b825bbe2ae97fd8c1bcd9f5b7
Ruth-ikegah <ruthikegah@gmail.com>
2020-08-24 16:38:19 +0100

    Work in progress
    Signed-off-by: Ruth-ikegah <ruthikegah@gmail.com>

--- my_site/Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn my_site.wsgi
--- my_site/settings.py
+++ my_site/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEYS')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://blog-experience.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -131,4 +130,3 @@ LOGIN_REDIRECT_URL = '/HomePage/'
 
 LOGIN_URL = '/login/'
 
-django_heroku.settings(locals())
